2aaa6013b24c73995beb4c083fa0571c24d0f41f
Divyanshu Batta <sumanbatta151@gmail.com>
2022-06-15 22:56:56 +0530

    Added new Calculators on the Home Page

--- All in one Calculator/All in One Calculator.js
+++ All in one Calculator/All in One Calculator.js
@@ -21,17 +21,17 @@ var time = new Date().getHours();
   if (time < 12) {
     document.getElementById(
       "salutation"
-    ).innerHTML = `<h2>Good Morning, ${username}</h2>`;
+    ).innerHTML = `<h2>Good Morning, ${username}👋</h2>`;
   } 
   else if (time > 12 && time <= 16) {
     document.getElementById(
       "salutation"
-    ).innerHTML = `<h2>Good Afternoon, ${username}</h2>`;
+    ).innerHTML = `<h2>Good Afternoon, ${username}👋</h2>`;
   } 
   else if (time >= 16 && time <= 24) {
     document.getElementById(
       "salutation"
-    ).innerHTML = `<h2>Good Evening, ${username}</h2>`;
+    ).innerHTML = `<h2>Good Evening, ${username}👋</h2>`;
   }
 }
 function animation() {
@@ -51,3 +51,34 @@ function random() {
   return parseInt(Math.random() * randomNum.length);
 }
 profile.src = `https://avatars.dicebear.com/4.9/api/micah/:seed${random()}.svg`;
+
+
+let button_finance_1_calc = document.getElementById("button_finance_1_calc");
+let Finance_1_result = document.getElementById("Finance_1_result");
+let calculate = () => {
+  let p = Number(document.getElementById("Finance_1_input_1").value);
+  let r = Number(document.getElementById("Finance_1_input_2").value);
+  let t = Number(document.getElementById("Finance_1_input_3").value);
+  let duration = document.getElementById("duration").value;
+
+  if (duration == "year") {
+    amount = p * (Math.pow((1 + r / 100), t));
+  } 
+  else if (duration == "month") {
+    amount = p * (Math.pow((1 + r / 100), (t/12)));
+  } 
+  else if (duration == "day") {
+    amount = p * (Math.pow((1 + r / 100), (t/365)));
+  } 
+let compoundInterest = amount - p;
+
+  Finance_1_result.innerHTML = `<div>Principal Amount: <span><i class="fa-solid fa-indian-rupee-sign"></i>${p.toFixed(2)}</span></div>
+  <div>Total Compound Interest: <span><i class="fa-solid fa-indian-rupee-sign"></i>${compoundInterest.toFixed(2)}</span></div>
+  <div>Total Amount: <span><i class="fa-solid fa-indian-rupee-sign"></i>${amount.toFixed(2)}</span></div>`;
+Finance_1_result.addEventListener("load", calculate);
+}
+
+function buttonDisappear() {
+  let button = document.getElementById("button_finance_1_calc");
+   button.style.display = "none";
+   }
